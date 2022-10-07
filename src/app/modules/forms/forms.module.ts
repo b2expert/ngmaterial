4d9d82cb45c1d09095286ef0d53e54fd0df3eeed
Biswajit Jana <beginner2expert4u@gmail.com>
2022-10-07 14:10:24 +0530

    basic config

--- src/app/modules/forms/forms.module.ts
+++ src/app/modules/forms/forms.module.ts
@@ -4,13 +4,15 @@ import { FormsComponent } from './forms.component';
 import { TemplateDrivenComponent } from './template-driven/template-driven.component';
 import { SharedModule } from 'src/app/shared.module';
 import { RouterModule } from '@angular/router';
+import { ReactiveSimpleComponent } from './reactive-simple/reactive-simple.component';
 
 
 
 @NgModule({
   declarations: [
     FormsComponent,
-    TemplateDrivenComponent
+    TemplateDrivenComponent,
+    ReactiveSimpleComponent
   ],
   imports: [
     SharedModule,
---
+++ src/app/modules/forms/reactive-simple/reactive-simple.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-reactive-simple',
+  templateUrl: './reactive-simple.component.html',
+  styleUrls: ['./reactive-simple.component.scss']
+})
+export class ReactiveSimpleComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
